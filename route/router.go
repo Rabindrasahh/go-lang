b4ed340aeb1a631f3611392bdd53e6f940bab2ce
Rabi Sah <rabindrasah1997.1@gmail.com>
2024-07-21 23:50:14 +0545

    User Create and Encryption helper added

--- route/router.go
+++ route/router.go
@@ -9,6 +9,7 @@ import (
 
 func RegisterRoutes(r *mux.Router, uc *controller.UserController) {
 	r.HandleFunc("/users", uc.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", uc.CreateUserHandler).Methods("POST")
 	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 	})
